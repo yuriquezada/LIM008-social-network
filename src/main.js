f08eb5a207e2f69a9735ab69e26c78f945f3beaa
brendapsd <brenda.sd.15@gmail.com>
2019-01-28 11:58:22 -0500

    Practicando login con firebase

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 // Este es el punto de entrada de tu aplicacion
 
-import { myFunction } from './lib/index.js';
+// import { myFunction } from './lib/index.js';
 
-myFunction();
+// myFunction();
+
+const btnRegistro = document.getElementById('btn-registro'); 
+
+btnRegistro.addEventListener('click', () =>{
+ const email = document.getElementById('email').value;
+ const password = document.getElementById('password').value;
+  console.log(email); 
+  console.log(password)
+
+ firebase.auth().createUserWithEmailAndPassword(email, password)
+ .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+    console.log(errorCode); 
+    console.log(errorMessage);
+  });
+}); 
