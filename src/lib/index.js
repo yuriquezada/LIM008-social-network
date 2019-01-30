67320e5250f11fe2305f4fb47f6e997e97ddde3a
brendapsd <brenda.sd.15@gmail.com>
2019-01-29 19:14:31 -0500

    Separación de la logica y el dom con los archivos index.js y main.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,12 @@
 // aqui exportaras las funciones que necesites
 
-export const myFunction = () => {
+export const signUp = (email, password) => {
   // aqui tu codigo
-}
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+ .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
+  });
+}; 
--- src/main.js
+++ src/main.js
@@ -1,90 +1,72 @@
 // Este es el punto de entrada de tu aplicacion
 
-// import { myFunction } from './lib/index.js';
+import { signUp } from './lib/index.js';
 
-// myFunction();
+const btnSignUp = document.getElementById('btn-sign-up'); 
 
-const btnRegistro = document.getElementById('registrarse'); 
-
-btnRegistro.addEventListener('click', () => {
+btnSignUp.addEventListener('click', () => {
  const email = document.getElementById('email').value;
  const password = document.getElementById('password').value;
-
- firebase.auth().createUserWithEmailAndPassword(email, password)
- .catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-  });
-  // document.getElementById('iniciar-sesion').innerHTML = `
-  // <h3>Iniciar Sesión</h3>
-  // <form action="">
-  //     <input id="email2" type="email" placeholder="Ingresa e-mail">
-  //     <input id="password2" type="password" placeholder="Ingresa contraseña">
-  //     <input id= "iniciar-sesion" type="button" value="Iniciar Sesión">
-  // </form>
-  // `
+ signUp(email, password);
 }); 
 
-const btnInicio = document.getElementById('iniciar-sesion'); 
+// const btnInicio = document.getElementById('iniciar-sesion'); 
 
-btnInicio.addEventListener('click', () => {
-  const email2 = document.getElementById('email2').value;
-  const password2 = document.getElementById('password2').value;
+// btnInicio.addEventListener('click', () => {
+//   const email2 = document.getElementById('email2').value;
+//   const password2 = document.getElementById('password2').value;
 
-  firebase.auth().signInWithEmailAndPassword(email2, password2).
-  catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    console.log(errorCode);
-    console.log(errorMessage);
-    // ...
-  });
-}); 
+//   firebase.auth().signInWithEmailAndPassword(email2, password2).
+//   catch(function(error) {
+//     // Handle Errors here.
+//     var errorCode = error.code;
+//     var errorMessage = error.message;
+//     console.log(errorCode);
+//     console.log(errorMessage);
+//     // ...
+//   });
+// }); 
 
-function observador() {
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      console.log('Existe usuario activo');
-      aparece()
-      // User is signed in.
-      var displayName = user.displayName;
-      var email = user.email;
-      console.log(email); 
-      var emailVerified = user.emailVerified;
-      var photoURL = user.photoURL;
-      var isAnonymous = user.isAnonymous;
-      var uid = user.uid;
-      var providerData = user.providerData;
-      // ...
-    } else {
-      // User is signed out.
-      console.log('No existe usuario activo'); 
-      // ...
-    }
-  });
-}; 
-observador(); 
+// function observador() {
+//   firebase.auth().onAuthStateChanged(function(user) {
+//     if (user) {
+//       console.log('Existe usuario activo');
+//       aparece()
+//       // User is signed in.
+//       var displayName = user.displayName;
+//       var email = user.email;
+//       console.log(email); 
+//       var emailVerified = user.emailVerified;
+//       var photoURL = user.photoURL;
+//       var isAnonymous = user.isAnonymous;
+//       var uid = user.uid;
+//       var providerData = user.providerData;
+//       // ...
+//     } else {
+//       // User is signed out.
+//       console.log('No existe usuario activo'); 
+//       // ...
+//     }
+//   });
+// }; 
+// observador(); 
 
-function aparece() {
-  const contenido = document.getElementById('contenido');
-  contenido.innerHTML = `
-  <p>¡Bienvenido!</p>
-  <button onclick="cerrar()">Cerrar Sesión</button>
-  `; 
-}
+// function aparece() {
+//   const contenido = document.getElementById('contenido');
+//   contenido.innerHTML = `
+//   <p>¡Bienvenido!</p>
+//   <button onclick="cerrar()">Cerrar Sesión</button>
+//   `; 
+// }
 
-const cerrar = () => {
-  firebase.auth().signOut()
-  .then(function(){
-    console.log('Saliendo...')
-  })
-  .catch(function(error){
-    console.log(error)
-  })
-  contenido.style.display = 'none'
-}; 
- 
+// const cerrar = () => {
+//   firebase.auth().signOut()
+//   .then(function(){
+//     console.log('Saliendo...')
+//   })
+//   .catch(function(error){
+//     console.log(error)
+//   })
+//   contenido.style.display = 'none'
+// }; 
  
