d70e295868aed2c6fd40f74b8d42b9c851d98adf
brendapsd <brenda.sd.15@gmail.com>
2019-02-08 17:57:25 -0500

    muestro el home al iniciar sesión y cierro sesion con el icono

--- src/lib/templateHome.js
+++ src/lib/templateHome.js
@@ -1,22 +1,58 @@
 import { signOut } from './firebase.js';
 
 export const home = () => {
-    const bienvenido = `
-    <h1>Bienvenido</h1>
-    <button class='btn-signout'>Cerrar Sesión</button>
+    const viewHome = `
+    <header class="menu-arriba color">
+        <img class="" src="imgs/Logo-P.png" alt="home">
+    </header>
+    <section>
+      <form action="">
+        <textarea name="" id="" placeholder="Escribe aquí..." cols="80" rows="8"></textarea>
+        <select class="button button1">
+          <option value="amigos"><img src="imgs/amigos-s.png" alt="iconA">Amigos</option>
+          <option value="privado"><img src="" alt="">Privado</option>
+        </select>
+        <button id="button-post" class="button button2">Publicar</button>
+      </form>
+      <table>
+        <tbody id="table">
+          <tr>
+          /aqui va el doc.id :D/
+          <td><button type="button" id="delete">Eliminar</button></td>
+          <td><button type="button" id="edit">Editar</button></td>
+          </tr>
+        </tbody>
+      </table>
+    <footer id="menu-abajo" class="menu-abajo color">
+      <img class="icon-signout" src="imgs/publico.png" alt="privado">
+      <img  src="imgs/amigos.png" alt="público">
+      <a href="#/signIn"><img id="sign-out" src="imgs/salir.png" alt="salir"></a>
+    </footer>
+    </section>
     `;
     const divElem = document.createElement('div');
-    divElem.setAttribute('id', 'bienvenido'); 
-    divElem.innerHTML = bienvenido;
+    divElem.setAttribute('id', 'muro');
+    divElem.innerHTML = viewHome;
     divElem
-     .querySelector('.btn-signout')
+     .querySelector('#sign-out')
      .addEventListener('click', () => { 
        signOut();     
-      //  divElem.style.display='none';     
-      //   document.getElementById('container').style.display='block';
      });
- 
-    //  const contenido = document.getElementById('conteiner');
-    //  contenido.appendChild(divElem); 
+    divElem
+      .querySelector('#button-post')
+      .addEventListener('click', () => { 
+       console.log('si funciono :D')     
+     });
+     divElem
+     .querySelector('#delete')
+     .addEventListener('click', () => { 
+      console.log('no se si funciono :S')     
+    });
+    divElem
+      .querySelector('#edit')
+      .addEventListener('click', () => { 
+       console.log('no se si funciono :S')     
+     });
+    
     return divElem; 
    }; 
