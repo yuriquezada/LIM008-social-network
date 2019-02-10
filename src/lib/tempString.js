6d22c777b883b1d25475c796fd33be5cf1decba0
yuriquezada <yuri_quezada@hotmail.com>
2019-02-10 02:46:42 -0500

    Muestra maquetado y estilo de sign in y sign up completos.

--- src/lib/tempString.js
+++ src/lib/tempString.js
@@ -22,13 +22,13 @@ export const home = () => {
 
 export const pageSignIn = () => {
   const template = `<form class="login">
-                      <img src="imgs/logo.png" alt="pets">
-                      <input class= "space" id="email2" type="email" placeholder="Ingresa e-mail" required>
-                      <input class= "space" id="password2" type="password" placeholder="Ingresa contraseña" required>
+                      <img class="logo" src="imgs/logo.png" alt="pets">
+                      <input class= "space" id="email2" type="email" placeholder="Correo electrónico" required>
+                      <input class= "space" id="password2" type="password" placeholder="Contraseña" required>
                       <div id="message"><P>Usuario o contraseña incorrectos</P></div>
                       <button class="button space" id= "btn-log-in">Iniciar sesión</button>
                       <button class="button google space" id= "google-button"><img src="imgs/google-logo.png" alt="Google">Iniciar sesión con Google</button>
-                      <img src="imgs/pets.png" alt="pets">
+                      <img class="pets" src="imgs/pets.png" alt="pets">
                       <p>¿No tienes cuenta? <a id="registrate" href="#">Regístrate</a></p>  
                     </form>`
   const element = document.createElement('div');
@@ -63,13 +63,14 @@ export const pageSignIn = () => {
 
 export const pageSignUp = () => {
   const template = `<form class="register">
+                      <img src="imgs/mini-logo.png" alt="pets">
                       <h2>Crea tu cuenta</h2>
                       <input class= "space" id="usser" type="text" placeholder="Elige tu usuario" required>
                       <input class= "space" id="email" type="email" placeholder="Ingresa e-mail" required>
                       <input class= "space" id="password" type="password" placeholder="Ingresa contraseña" required>
                       <div id="message2"><P>No se ingreso usuario, e-mail o contraseña</P></div>
                       <button class="button space" id= "btn-sign-up">Registrar</button>
-                      </form>`
+                    </form>`
   const element2 = document.createElement('div');
   element2.innerHTML = template;
 
@@ -80,7 +81,7 @@ export const pageSignUp = () => {
       const email = document.querySelector("#email").value;
       const password = document.querySelector("#password").value;
       signUp(email, password);
-      changeHash('/signin')
+      // changeHash('/signin')
     });
 
 
