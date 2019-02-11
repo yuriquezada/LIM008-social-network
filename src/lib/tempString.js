8359fbbb858449fad760157d5ab1927353c6a3b1
yuriquezada <yuri_quezada@hotmail.com>
2019-02-11 18:01:31 -0500

    Proceso de responsive de signup y signin

--- src/lib/tempString.js
+++ src/lib/tempString.js
@@ -23,13 +23,20 @@ export const home = () => {
 export const pageSignIn = () => {
   const template = `<form class="login">
                       <img class="logo" src="imgs/logo.png" alt="pets">
-                      <input class= "space" id="email2" type="email" placeholder="Correo electrónico" required>
-                      <input class= "space" id="password2" type="password" placeholder="Contraseña" required>
-                      <div id="message"><P>Usuario o contraseña incorrectos</P></div>
-                      <button class="button space" id= "btn-log-in">Iniciar sesión</button>
-                      <button class="button google space" id= "google-button"><img src="imgs/google-logo.png" alt="Google">Iniciar sesión con Google</button>
-                      <img class="pets" src="imgs/pets.png" alt="pets">
-                      <p>¿No tienes cuenta? <a id="registrate" href="#">Regístrate</a></p>  
+                      <section class="inputs-buttons-img">
+                        <div class="inputs-buttons">
+                          <input class= "space" id="email2" type="email" placeholder="Correo electrónico" required>
+                          <input class= "space" id="password2" type="password" placeholder="Contraseña" required>
+                          <div id="message"><P>Usuario o contraseña incorrectos</P></div>
+                          <button class="button space" id= "btn-log-in">Iniciar sesión</button>
+                          <button class="button google space" id= "google-button"><img src="imgs/google-logo.png" alt="Google">Iniciar sesión con Google</button>
+                        </div>
+                        <div class="pets-signup">
+                          <img class="pets" src="imgs/pets.png" alt="pets">
+                          <p>¿No tienes cuenta? <a id="registrate" href="#">Regístrate</a></p>  
+                        </div>
+                      </section>
+
                     </form>`
   const element = document.createElement('div');
   element.innerHTML = template;
@@ -64,12 +71,14 @@ export const pageSignIn = () => {
 export const pageSignUp = () => {
   const template = `<form class="register">
                       <img src="imgs/mini-logo.png" alt="pets">
-                      <h2>Crea tu cuenta</h2>
-                      <input class= "space" id="usser" type="text" placeholder="Elige tu usuario" required>
-                      <input class= "space" id="email" type="email" placeholder="Ingresa e-mail" required>
-                      <input class= "space" id="password" type="password" placeholder="Ingresa contraseña" required>
-                      <div id="message2"><P>No se ingreso usuario, e-mail o contraseña</P></div>
-                      <button class="button space" id= "btn-sign-up">Registrar</button>
+                      <div class="inputs-buttons">
+                        <h2>Crea tu cuenta</h2>
+                        <input class= "space" id="usser" type="text" placeholder="Elige tu usuario" required>
+                        <input class= "space" id="email" type="email" placeholder="Ingresa e-mail" required>
+                        <input class= "space" id="password" type="password" placeholder="Ingresa contraseña" required>
+                        <div id="message2"><P>No se ingreso usuario, e-mail o contraseña</P></div>
+                        <button class="button space" id= "btn-sign-up">Registrar</button>
+                      </div>
                     </form>`
   const element2 = document.createElement('div');
   element2.innerHTML = template;
