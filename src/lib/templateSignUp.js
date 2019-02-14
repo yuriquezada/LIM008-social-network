4b70c02960d39444113acdce662e6ab9220530e5
yuriquezada <yuri_quezada@hotmail.com>
2019-02-14 16:57:00 -0500

    Contiene ordenado de archivos provenientes de la rama master

--- src/lib/templateSignUp.js
+++ src/lib/templateSignUp.js
@@ -1,6 +1,7 @@
-import { signUp } from './firebase.js';
+import { signUpOnSubmit } from './view-controller.js';
 
 export const pageSignUp = () => {
+    const formElemt2 = document.createElement('form');
     const template = `<form class="register">
                         <img src="imgs/mini-logo.png" alt="pets">
                         <div class="inputs-buttons">
@@ -12,17 +13,12 @@ export const pageSignUp = () => {
                               <button class="button space" id= "btn-sign-up">Registrar</button>
                         </div>
                   </form>`
-    const element2 = document.createElement('div');
-    element2.innerHTML = template;
-
-    element2 
-     .querySelector('#btn-sign-up')
-     .addEventListener('click', event => {
-           event.preventDefault();
-          const email = document.querySelector("#email").value;
-          const password = document.querySelector("#password").value;
-            signUp(email, password);
-         });
-
-      return element2;
+    
+      formElemt2.classList.add('register')
+      formElemt2.innerHTML = template;
+      const btnRegister = formElemt2.querySelector('#btn-sign-up');
+      btnRegister.addEventListener( 'click', e => {
+      e.preventDefault();
+      signUpOnSubmit()});
+      return formElemt2;
 };
--- src/lib/view-controller.js
+++ src/lib/view-controller.js
@@ -1,3 +1,36 @@
-import {deletePost } from "../firebase.js"
-export const deleteNote = (objNote) =>
+import { logIn, deletePost, signUp, checkEmail, signOut, showPost, googleLogIn } from "./controller.js";
+import { changeHash } from "../route.js";
+
+export const signInOnSubmit = () => {
+    const email = document.querySelector('#email2').value;
+    const password = document.querySelector('#password2').value;
+    logIn(email, password)
+        .then(() => changeHash('/home'))
+        .catch(() => {document.querySelector("#message").style.display = "block";})
+}
+
+export const signInWithGoogle = () => {
+    googleLogIn()
+        .then(() => changeHash('/home'))
+}
+export const signUpOnSubmit = () => {
+    const email = document.querySelector("#email").value;
+    const password = document.querySelector("#password").value;
+    signUp(email, password)
+        .then(()=> {checkEmail(); changeHash('/home')} )
+        .catch (() => {document.getElementById("message2").style.display = "block";})
+}
+
+export const submitPost = () => {
+const textArea = document.getElementById('post');
+    showPost(textArea.value)
+}
+
+export const deleteOnClickPost = (objNote) =>
 deletePost(objNote.id)
+
+export const signOutOnClick = () => {
+    signOut()
+        .then(() => changeHash('/signIn'))
+}
+
